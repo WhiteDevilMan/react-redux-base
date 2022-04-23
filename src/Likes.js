98b15f22d199e98fd00a2f26e690ec97109593e8
Mikhail <mixail.workmail@yandex.ru>
2022-04-23 09:03:51 +0300

    feat: 🎸 add mapStateToProps and mapDispatchToProps

--- src/Likes.js
+++ src/Likes.js
@@ -1,11 +1,30 @@
+import { connect } from "react-redux";
+
 const Likes = (props) => {
-  console.log(props);
+  console.log('render > ', props);
   return (
     <div className='button-controls'>
-      <button>❤ {props.likes}</button>
+      <button onClick={props.onIncrementLikes}>❤ {props.likes}</button>
       <button>Dislike</button>
     </div>
   )
 }
 
-export default Likes;
+const mapStateToProps = (state) => {
+  console.log('mapStateToProps > ', state);
+  return {
+    likes: state.likes,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIncrementLikes: () => {
+      console.log('click');
+      const action = {type: 'INCREMENT'};
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes);
