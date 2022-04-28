f59353dc72b6226827ddfe2d76894328b7cead2c
Mikhail <mixail.workmail@yandex.ru>
2022-04-28 12:07:53 +0300

    feat: add display error

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import Likes from './Likes';
 import Title from './Title';
 import Comments from './Comments';
@@ -6,11 +7,19 @@ import Spin from './Spin';
 import './App.css';
 
 function App() {
+  const error = useSelector(state => state.appReducer.error);
+  console.log('error > ', error);
+
   return (
     <div className="App">
       <div className="wrap">
         <Spin />
         <div className="card">
+          {error && (
+            <div className='error-message'>
+              {error}
+            </div>
+          )}
           <div className="card-image">
             <img src="./sea.jpg" alt="surfing"/>
             <Title />
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -8,6 +8,8 @@ import {
   COMMENTS_LOAD,
   LOADER_DISPLAY_ON,
   LOADER_DISPLAY_OFF,
+  ERROR_DISPLAY_ON,
+  ERROR_DISPLAY_OFF,
 } from "./types";
 
 export const incrementLikes = () => {
@@ -62,20 +64,44 @@ export const loaderOff = () => {
   }
 }
 
-export const commentsLoad = () => {
-  return async dispatch => {
-    dispatch(loaderOn());
-    const response = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=10');
-    const jsonData = await response.json();
+export const errorOn = (text) => {
+  return dispatch => {
+    dispatch({
+      type: ERROR_DISPLAY_ON,
+      text,
+    })
 
     setTimeout(() => {
-      dispatch({
-        type: COMMENTS_LOAD,
-        data: jsonData,
-      });
+      dispatch(errorOff());
+    }, 5000);
+  }
+}
 
+export const errorOff = () => {
+  return {
+    type: ERROR_DISPLAY_OFF,
+  }
+}
+
+export const commentsLoad = () => {
+  return async dispatch => {
+    try {
+      dispatch(loaderOn());
+      const response = await fetch('https://wjsonplaceholder.typicode.com/comments?_limit=10');
+      const jsonData = await response.json();
+  
+      setTimeout(() => {
+        dispatch({
+          type: COMMENTS_LOAD,
+          data: jsonData,
+        });
+  
+        dispatch(loaderOff());
+      }, 3000);
+    } catch (err) {
+      dispatch(errorOn('Ошибка API'));
       dispatch(loaderOff());
-    }, 3000);
+    }
 
   }
 }
